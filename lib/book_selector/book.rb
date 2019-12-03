eff12fc226bfaca676f3be352a71d473c631b8e4
AnnaWijetunga <annawijetunga@gmail.com>
2019-12-03 10:40:45 -0500

    Add reader/writer properties to title, summary and url

--- lib/book_selector/book.rb
+++ lib/book_selector/book.rb
@@ -2,22 +2,26 @@
 
 class BookSelector::Book
 
+  attr_accessor :title, :summary, :url
+
   def self.all
     # I should return instances of books
     puts "Classic Kid's Books"
     puts <<-DOC
-      1. "Adventures of Huckleberry Finn"
-      2. "Alice's Adventures in Wonderland"
-      3. "Fairy Tales of the Brothers Grimm"
-      4. "Hansel & Grethel"
-      5. "The Jungle Book"
-      6. "Pinocchio"
-      7. "Robin Hood"
-      8. "Snow White"
-      9. "Treasure Island"
-      10. "The Wonderful Wizard of Oz "
+      1. "Adventures of Huckleberry Finn" - This great American novel follows the adventures of a teenager and his life on the Mississippi River.
+      2. "Alice's Adventures in Wonderland" - A girl named Alice falls into a rabbit hole, where she encounters a world of strange creatures.
     DOC
-  end 
+    book_1 = self.new # self is Book here
+    book_1.title = "'Adventures of Huckleberry Finn'"
+    book_1.summary = "This great American novel follows the adventures of a teenager and his life on the Mississippi River."
+    book_1.url = "http://read.gov/books/"
 
+    book_2 = self.new # self is Book here
+    book_2.title = "'Alice's Adventures in Wonderland'"
+    book_2.summary = "A girl named Alice falls into a rabbit hole, where she encounters a world of strange creatures."
+    book_2.url = "http://read.gov/books/"
+    
+    [book_1, book_2]
+  end 
 
 end 
