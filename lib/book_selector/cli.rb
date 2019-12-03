5e3ab54c1e29802f812413b77da290b7aca11385
AnnaWijetunga <annawijetunga@gmail.com>
2019-12-03 09:52:41 -0500

    Only run our hard-coded data in our new book file and class

--- lib/book_selector/cli.rb
+++ lib/book_selector/cli.rb
@@ -8,19 +8,19 @@ class BookSelector::CLI
   end
 
   def list_books
-    puts "Classic Kid's Books"
-    puts <<-DOC
-      1. "Adventures of Huckleberry Finn"
-      2. "Alice's Adventures in Wonderland"
-      3. "Fairy Tales of the Brothers Grimm"
-      4. "Hansel & Grethel"
-      5. "The Jungle Book"
-      6. "Pinocchio"
-      7. "Robin Hood"
-      8. "Snow White"
-      9. "Treasure Island"
-      10. "The Wonderful Wizard of Oz "
-    DOC
+    # puts "Classic Kid's Books"
+    # puts <<-DOC
+    #   1. "Adventures of Huckleberry Finn"
+    #   2. "Alice's Adventures in Wonderland"
+    #   3. "Fairy Tales of the Brothers Grimm"
+    #   4. "Hansel & Grethel"
+    #   5. "The Jungle Book"
+    #   6. "Pinocchio"
+    #   7. "Robin Hood"
+    #   8. "Snow White"
+    #   9. "Treasure Island"
+    #   10. "The Wonderful Wizard of Oz "
+    # DOC
     @books = BookSelector::Book.all # time to make a new file, book
   end 
 
