19d734607338d5a16a2913060e9d12191ae26f8d
AnnaWijetunga <annawijetunga@gmail.com>
2019-12-03 22:29:41 -0500

    Scrape title, summary and link for all books. Require open-uri and nokogiri

--- lib/book_selector/book.rb
+++ lib/book_selector/book.rb
@@ -1,4 +1,6 @@
 # I will have to require this new file
+require "open-uri"
+require "nokogiri"
 
 class BookSelector::Book
 
@@ -10,9 +12,38 @@ class BookSelector::Book
     self.scrape_books
     # BookSelector::BookScraper.new("http://read.gov/books/")
     # BookSelector::BookScraper.new.books
+
+  # p > strong
+    
   end 
 
-  def self.scrape_books
+  def self.scrape_books 
+    base_url = "http://read.gov/books/"
+    html = open(base_url).read 
+    page = Nokogiri::HTML.parse(html)
+    
+    page.css("#main_body > ul:nth-child(6) > li").map do |book_node|
+      book = new
+
+      title_node = book_node.css("strong")
+      book.title = title_node.text
+
+      read_more_url_node = book_node.css("p > a").find do |node|
+        node.text == "More About this Book"
+      end
+     
+      relative_path = read_more_url_node.attribute("href")
+      url = "#{base_url}#{relative_path}"
+      book.url = url 
+      
+      book_text = book_node.css("p").to_s
+      summary = book_text.split("<br>")[1].strip
+      book.summary = summary
+      book
+    end
+  end
+
+  def self.create_books
     # go to read.gov
     # find the books
     # extract the properties
@@ -35,4 +66,8 @@ class BookSelector::Book
     [book_1, book_2]
   end 
 
+  def to_s 
+    "Book: #{title}, summary: #{summary}, url: #{url}"
+  end
 end 
+
