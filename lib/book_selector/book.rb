1cd33826980ac988b700be416f97520555c4d27c
AnnaWijetunga <annawijetunga@gmail.com>
2019-12-03 20:15:03 -0500

    Display hard-coded data

--- lib/book_selector/book.rb
+++ lib/book_selector/book.rb
@@ -12,27 +12,27 @@ class BookSelector::Book
     # BookSelector::BookScraper.new.books
   end 
 
-  def self.scrape_deals
-    books = []
+  def self.scrape_books
     # go to read.gov
     # find the books
     # extract the properties
     # instantiate the book
-    
-    books # an array of books I just scraped
+
+    # books = []
+    # books # an array of books I just scraped
 
     # my old, hard-coded data:
-    # book_1 = self.new # self is Book here
-    # book_1.title = "'Adventures of Huckleberry Finn'"
-    # book_1.summary = "This great American novel follows the adventures of a teenager and his life on the Mississippi River."
-    # book_1.url = "http://read.gov/books/"
+    book_1 = self.new # self is Book here
+    book_1.title = "'Adventures of Huckleberry Finn'"
+    book_1.summary = "This great American novel follows the adventures of a teenager and his life on the Mississippi River."
+    book_1.url = "http://read.gov/books/"
 
-    # book_2 = self.new # self is Book here
-    # book_2.title = "'Alice's Adventures in Wonderland'"
-    # book_2.summary = "A girl named Alice falls into a rabbit hole, where she encounters a world of strange creatures."
-    # book_2.url = "http://read.gov/books/"
+    book_2 = self.new # self is Book here
+    book_2.title = "'Alice's Adventures in Wonderland'"
+    book_2.summary = "A girl named Alice falls into a rabbit hole, where she encounters a world of strange creatures."
+    book_2.url = "http://read.gov/books/"
     
-    # [book_1, book_2]
+    [book_1, book_2]
   end 
 
 end 
