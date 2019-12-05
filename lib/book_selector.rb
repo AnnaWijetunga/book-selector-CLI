30d36e01553b2fdf0ec2ac048b44f36b19e85e4e
AnnaWijetunga <annawijetunga@gmail.com>
2019-12-05 08:33:21 -0500

    Delete unnecessary comments - declutter the file

--- lib/book_selector.rb
+++ lib/book_selector.rb
@@ -1,14 +1,5 @@
-# require 'nokogiri'
-# require 'pry'
+# this file is my environment
 
 require_relative "./book_selector/version"
 require_relative "./book_selector/book"
 require_relative "./book_selector/cli" 
-
-# this file acts as my environment
-
-# deleting this module below - it's really housed in my version file
-
-# module BookSelector
-#   class Error < StandardError; end
-# end
