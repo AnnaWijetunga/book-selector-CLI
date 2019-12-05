423ade17b52a22531538da27353c5385900b7958
AnnaWijetunga <annawijetunga@gmail.com>
2019-12-05 13:08:50 -0500

    Move scraping data to scraper file

--- lib/book_selector/book.rb
+++ lib/book_selector/book.rb
@@ -5,41 +5,38 @@ require "pry"
 
 class BookSelector::Book
 
-  attr_accessor :title, :summary, :url
+  attr_accessor :title, :summary, :url, :author
 
-  def self.all # return all instances of books after scraping read.gov
-    self.scrape_books
-  end 
+  # def self.all # return all instances of books after scraping read.gov
+  #   self.scrape_books
+  # end 
 
-  def self.scrape_books 
-    base_url = "http://read.gov/books/"
-    html = open(base_url).read 
-    page = Nokogiri::HTML.parse(html)
-    # doc = Nokogiri::HTML(open("http://read.gov/books/"))
+  # def self.scrape_books 
+  #   base_url = "http://read.gov/books/"
+  #   html = open(base_url).read 
+  #   page = Nokogiri::HTML.parse(html)
+  #   # doc = Nokogiri::HTML(open("http://read.gov/books/"))
 
-    page.css("#main_body > ul:nth-child(6) > li").map do |book_node|
-      book = new
+  #   page.css("#main_body > ul:nth-child(6) > li").map do |book_node|
+  #     book = new
       
-      title_node = book_node.css("strong")
-      book.title = title_node.text
+  #     title_node = book_node.css("strong")
+  #     book.title = title_node.text
       
-      read_more_url_node = book_node.css("p > a").find do |node|
-        node.text == "More About this Book"
-      end
+  #     read_more_url_node = book_node.css("p > a").find do |node|
+  #       node.text == "More About this Book"
+  #     end
      
-      relative_path = read_more_url_node.attribute("href")
-      url = "#{base_url}#{relative_path}"
-      book.url = url 
+  #     relative_path = read_more_url_node.attribute("href")
+  #     url = "#{base_url}#{relative_path}"
+  #     book.url = url 
       
-      book_text = book_node.css("p").to_s
-      summary = book_text.split("<br>")[1].strip
-      book.summary = summary
-      book
-    end
-  end
-
-  # def to_s 
-  #   "Book: #{title}, summary: #{summary}, url: #{url}"
+  #     book_text = book_node.css("p").to_s
+  #     summary = book_text.split("<br>")[1].strip
+  #     book.summary = summary
+  #     book
+  #   end
   # end
+
 end 
 
