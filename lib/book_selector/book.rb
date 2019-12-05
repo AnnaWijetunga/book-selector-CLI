1a320ab445ac4a824c63753925cf7b9a9602b9e3
AnnaWijetunga <annawijetunga@gmail.com>
2019-12-05 13:05:07 -0500

    Keep scraping under one method for now

--- lib/book_selector/book.rb
+++ lib/book_selector/book.rb
@@ -38,24 +38,8 @@ class BookSelector::Book
     end
   end
 
-  # def self.scrape_urls
-  #   base_url = "http://read.gov/books/"
-  #   html = open(base_url).read 
-  #   page = Nokogiri::HTML.parse(html) 
-
-  #   page.css("#main_body > ul:nth-child(6) > li").map do |book_node|
-  #     read_more_url_node = book_node.css("p > a").find do |node|
-  #       node.text == "More About this Book"
-  #     end
-     
-  #     relative_path = read_more_url_node.attribute("href")
-  #     url = "#{base_url}#{relative_path}"
-  #     book.url = url 
-  #   end
+  # def to_s 
+  #   "Book: #{title}, summary: #{summary}, url: #{url}"
   # end
-
-  def to_s 
-    "Book: #{title}, summary: #{summary}, url: #{url}"
-  end
 end 
 
