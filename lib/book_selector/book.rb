c9774bd0b8affc2bd2e326b43be1e054a58b472d
AnnaWijetunga <annawijetunga@gmail.com>
2019-12-05 12:53:04 -0500

    Remove author from attr_accessor list

--- lib/book_selector/book.rb
+++ lib/book_selector/book.rb
@@ -5,7 +5,7 @@ require "pry"
 
 class BookSelector::Book
 
-  attr_accessor :title, :summary, :url, :author
+  attr_accessor :title, :summary, :url
 
   def self.all # return all instances of books after scraping read.gov
     self.scrape_books
@@ -38,21 +38,21 @@ class BookSelector::Book
     end
   end
 
-  def self.scape_url
-    base_url = "http://read.gov/books/"
-    html = open(base_url).read 
-    page = Nokogiri::HTML.parse(html) 
+  # def self.scrape_urls
+  #   base_url = "http://read.gov/books/"
+  #   html = open(base_url).read 
+  #   page = Nokogiri::HTML.parse(html) 
 
-    page.css("#main_body > ul:nth-child(6) > li").map do |book_node|
-      read_more_url_node = book_node.css("p > a").find do |node|
-        node.text == "More About this Book"
-      end
+  #   page.css("#main_body > ul:nth-child(6) > li").map do |book_node|
+  #     read_more_url_node = book_node.css("p > a").find do |node|
+  #       node.text == "More About this Book"
+  #     end
      
-      relative_path = read_more_url_node.attribute("href")
-      url = "#{base_url}#{relative_path}"
-      book.url = url 
-    end
-  end
+  #     relative_path = read_more_url_node.attribute("href")
+  #     url = "#{base_url}#{relative_path}"
+  #     book.url = url 
+  #   end
+  # end
 
   def to_s 
     "Book: #{title}, summary: #{summary}, url: #{url}"
