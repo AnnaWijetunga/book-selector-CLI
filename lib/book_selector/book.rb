f8e5a4d98a02ce260eaefcefeb3126cc87c80d29
AnnaWijetunga <annawijetunga@gmail.com>
2019-12-05 12:05:30 -0500

    Scrape the url for more info about a book

--- lib/book_selector/book.rb
+++ lib/book_selector/book.rb
@@ -38,6 +38,22 @@ class BookSelector::Book
     end
   end
 
+  def self.scape_url
+    base_url = "http://read.gov/books/"
+    html = open(base_url).read 
+    page = Nokogiri::HTML.parse(html) 
+
+    page.css("#main_body > ul:nth-child(6) > li").map do |book_node|
+      read_more_url_node = book_node.css("p > a").find do |node|
+        node.text == "More About this Book"
+      end
+     
+      relative_path = read_more_url_node.attribute("href")
+      url = "#{base_url}#{relative_path}"
+      book.url = url 
+    end
+  end
+
   def to_s 
     "Book: #{title}, summary: #{summary}, url: #{url}"
   end
