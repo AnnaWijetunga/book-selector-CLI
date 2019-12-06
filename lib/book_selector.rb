6ea48dedadcf1171cfae25c6bc7ec7363ecf4c15
AnnaWijetunga <annawijetunga@gmail.com>
2019-12-06 07:53:44 -0500

    Require scraper file

--- lib/book_selector.rb
+++ lib/book_selector.rb
@@ -1,5 +1,7 @@
 # this file is my environment
 
 require_relative "./book_selector/version"
+require_relative "./book_selector/scraper"
 require_relative "./book_selector/book"
 require_relative "./book_selector/cli" 
+
