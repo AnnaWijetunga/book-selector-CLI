830b7bc75b3ce7fada22a95384038bcca00f1fbb
AnnaWijetunga <annawijetunga@gmail.com>
2019-12-09 20:45:20 -0500

    Notes to self about self

--- lib/book_selector/book.rb
+++ lib/book_selector/book.rb
@@ -1,17 +1,20 @@
 # I have had to require this new file in my book_selector.rb (environment) file
 
-require "open-uri"
+require "open-uri" # lets us open the HTML like a file and pass it to Nokogiri
 require "nokogiri"
 require "pry"
 
-class BookSelector::Book # for storing data (not scraping - moved that)
+class BookSelector::Book # defines how to build a book - and used for storing data about books 
 
   attr_accessor :title, :summary, :url
   @@all = []
 
-  def self.all
-    @@all
+  def self.all 
+    @@all # class variable that stores every book instance upon initialization.
   end 
+
+  # every object is aware of itself
+  # define methods in which we tell objects to operate on themselves
 
   def save
     @@all << self
@@ -23,3 +26,11 @@ class BookSelector::Book # for storing data (not scraping - moved that)
   end
 
 end 
+
+# def title  # getter method
+#   @title
+# end 
+
+# def title=title # setter method
+#   @title = title
+# end
