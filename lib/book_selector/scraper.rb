089b62a83101a8cc2a101fd6d17e1fab82dc33a8
AnnaWijetunga <annawijetunga@gmail.com>
2019-12-09 20:46:52 -0500

    Describe book instances

--- lib/book_selector/scraper.rb
+++ lib/book_selector/scraper.rb
@@ -6,7 +6,7 @@ require "pry"
 class BookSelector::Scraper
 
   def initialize(title = nil)
-    @title = title
+    @title = title # book title assigned to book as it's created
   end 
 
   def all # return all instances of books after scraping read.gov
@@ -15,13 +15,13 @@ class BookSelector::Scraper
 
   def scrape_books 
     base_url = "http://read.gov/books/"
-    html = open(base_url).read 
+    html = open(base_url).read # opens the site object and reads the whole file/contents
     page = Nokogiri::HTML.parse(html)
-    # doc = Nokogiri::HTML(open("http://read.gov/books/"))
+    # doc = Nokogiri::HTML(open("http://read.gov/books/")) - how I've seen this condensed
 
     page.css("#main_body > ul:nth-child(6) > li").each do |book_node|
       # book = new
-      book = BookSelector::Book.new
+      book = BookSelector::Book.new # instantiate instances of the book class object
       
       title_node = book_node.css("strong") # title
       book.title = title_node.text
@@ -37,7 +37,7 @@ class BookSelector::Scraper
       book_text = book_node.css("p").to_s # summary
       summary = book_text.split("<br>")[1].strip
       book.summary = summary
-      book.save
+      book.save # save all book instances so we can see them as output!
     end
   end 
 end
